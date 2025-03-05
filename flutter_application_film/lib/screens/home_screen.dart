280d5654e2f0e76904c38f4ba29e18bc0c243020
StevennTannn <tansteven662@gmail.com>
2025-03-05 09:20:43 +0700

    p5(1)

--- flutter_application_film/lib/screens/home_screen.dart
+++ flutter_application_film/lib/screens/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_application_film/models/movie.dart';
 import 'package:flutter_application_film/services/api_service.dart';
@@ -15,12 +17,71 @@ class _HomeScreenState extends State<HomeScreen> {
   List<Movie> _trendingMovie = [];
   List<Movie> _popularMovies = [];
 
-  Future<void> loadMovies() async {
-    final List<Map<String, dynamic>> allMoviesData = await apiService.getAllMovies();
+  Future<void> _loadMovies() async {
+    final List<Map<String, dynamic>> allMoviesData =
+        await apiService.getAllMovies();
+    final List<Map<String, dynamic>> trendMoviesData =
+        await apiService.getTrendingMovies();
+    final List<Map<String, dynamic>> popularMoviesData =
+        await apiService.getPopularMovies();
+
+    setState(() {
+      _allMovies = allMoviesData.map((e) => Movie.fromJson(e)).toList();
+      _trendingMovie = trendMoviesData.map((e) => Movie.fromJson(e)).toList();
+      _popularMovies = popularMoviesData.map((e) => Movie.fromJson(e)).toList();
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    _loadMovies();
+    super.initState();
+  }
+
+  Widget _buildMoviesListInterface(String title, List<Movie> movies) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            title,
+            style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+        ),
+        ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: movies.length,
+            itemBuilder: (BuildContext context, int index) {
+              final Movie movie = movies[index];
+              return Column(
+                children: [
+                  Image.network('https://image.tmdb.org/t/p/w500${movie.posterPath}',
+                  height: 150,
+                  width: 150,
+                  fit: BoxFit.cover,
+                  ),
+                  const SizedBox(height: 5,
+                  ),
+                  Text(movie.title, style: const TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  
+                ],
+              );
+            }),
+      ],
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Film'),
+      ),
+      body: Column(children: [_buildMoviesListInterface('All Movies', _allMovies)],
+      
+      ),
+    );
   }
 }
