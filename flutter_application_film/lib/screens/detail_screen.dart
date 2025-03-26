5f80d0223de81d15b552899a48644f999d306e38
StevennTannn <tansteven662@gmail.com>
2025-03-26 08:38:27 +0700

    p11

--- flutter_application_film/lib/screens/detail_screen.dart
+++ flutter_application_film/lib/screens/detail_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_film/models/movie.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class DetailScreen extends StatefulWidget {
   final Movie movie;
@@ -11,7 +14,43 @@ class DetailScreen extends StatefulWidget {
 }
 
 class _DetailScreenState extends State<DetailScreen> {
-  bool isFavorite = false;
+  bool _isFavorite = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _checkIsFavorite();
+  }
+
+  Future<void> _checkIsFavorite() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _isFavorite = prefs.containsKey('movie_${widget.movie.id}');
+    });
+  }
+
+  Future<void> _toggleFavorite() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _isFavorite = !_isFavorite;
+    });
+
+    if (_isFavorite) {
+      final String movieJson = jsonEncode(widget.movie.toJson());
+      print("Movie JSON " + movieJson);
+      prefs.setString('movie_${widget.movie.id}', movieJson);
+      List<String> favoriteMovieIds =
+          prefs.getStringList('favoriteMovies') ?? [];
+      favoriteMovieIds.add(widget.movie.id.toString());
+      prefs.setStringList('favoriteMovies', favoriteMovieIds);
+    } else {
+      prefs.remove('movie_${widget.movie.id}');
+      List<String> favoriteMovieIds =
+          prefs.getStringList('favoriteMovies') ?? [];
+      favoriteMovieIds.remove(widget.movie.id.toString());
+      prefs.setStringList('favoriteMovies', favoriteMovieIds);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,6 +69,18 @@ class _DetailScreenState extends State<DetailScreen> {
                   height: 300,
                   width: double.infinity,
                   fit: BoxFit.cover,
+                ),
+                Positioned(
+                  bottom: 8,
+                  right: 8,
+                  child: IconButton(
+                    icon: Icon(
+                      _isFavorite ? Icons.favorite : Icons.favorite_border,
+                      color: Colors.red,
+                    ),
+                    onPressed: _toggleFavorite,
+                    //onPressed: () {},
+                  ),
                 ),
                 const SizedBox(
                   height: 20,
